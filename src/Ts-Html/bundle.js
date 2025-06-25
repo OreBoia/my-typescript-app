80cdf3afc76a67c14c646d5fefd6a408b3cb555c
Edoardo Del Sarto <e99delsarto@gmail.com>
2025-06-25 15:06:37 +0200

    Update: + example of DOM manipulation

--- src/Ts-Html/bundle.js
+++ src/Ts-Html/bundle.js
@@ -4,4 +4,23 @@ const btn = document.getElementById("salutaBtn");
 const messaggio = document.getElementById("messaggio");
 btn.addEventListener("click", () => {
     messaggio.textContent = "Ciao, benvenuto nel mondo di TypeScript e HTML!";
+    const dynamicMessage = document.getElementById("dynamicMessage");
+    if (!dynamicMessage) {
+        const messaggio2 = document.createElement("p");
+        messaggio2.textContent = "Questo è un messaggio aggiunto dinamicamente.";
+        document.body.appendChild(messaggio2);
+        messaggio2.style.color = "blue";
+        messaggio2.className = "dynamic-message";
+        messaggio2.style.fontSize = "20px";
+        messaggio2.id = "dynamicMessage";
+    }
+    if (dynamicMessage) {
+        dynamicMessage.addEventListener("mouseover", () => {
+            console.log("Il messaggio dinamico è stato trascinato!");
+            dynamicMessage.style.backgroundColor = "lightyellow";
+            dynamicMessage.style.cursor = "pointer";
+            dynamicMessage.style.fontWeight = "bold";
+            dynamicMessage.style.textDecoration = "underline";
+        });
+    }
 });
